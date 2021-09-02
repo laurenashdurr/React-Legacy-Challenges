1ec500fa8b909717792ac3fd7f04ca32ad0b8c3b
Lauren Durr <laurenashdurr@gmail.com>
2021-09-02 17:28:58 -0400

    some of the logic for the searchindex

--- src/Components/Search/SearchIndex.js
+++ src/Components/Search/SearchIndex.js
@@ -1,21 +1,34 @@
-import React from 'react';
-import {Input} from 'reactstrap';
- 
-const SearchIndex extends Component() {
-   this.state = {
-     things: ['pen', 'marker', 'eraser', 'notebook', 'pencil', 'scissors', 'highlighter', 'stapler', 'paper clip', 'binder', 'hole punch', 'laminator', 'laminating sheets', 'protective sheets', 'index cards']
-   }
- }
+import React, { Component } from 'react';
+import { Input } from 'reactstrap';
 
- function searchFunction() {
- }
+class SearchIndex extends Component {
+  constructor() {
+    super()
+    this.state = {
+      things: ['pen', 'marker', 'eraser', 'notebook', 'pencil', 'scissors', 'highlighter', 'stapler', 'paper clip', 'binder', 'hole punch', 'laminator', 'laminating sheets', 'protective sheets', 'index cards']
+    }
+    console.log(this.state.things)
+  }
 
- render() {
-     <div>
-       <Input placeholder='Search Here' />
-       <h3>Results:</h3>
-     </div>
- }
+  
 
- 
-export SearchIndex;
+  
+  render() {
+    
+    //  function searchFunction() {
+    //   return this.state.thing == input
+    //  }
+
+  return (
+    <div>
+      <Input placeholder='Search Here' />
+      <h3>Results:</h3>
+      <h2>
+        {/* {this.state.things.filter(searchFunction)} */}
+      </h2>
+    </div>
+  )
+}
+}
+
+export default SearchIndex;
